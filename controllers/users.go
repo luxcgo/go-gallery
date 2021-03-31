b843e3a04180b50de1f623976400c8a1f4070be5
luxcgo <luxcgo@gmail.com>
2021-03-31 23:35:29 +0800

    controller/users: refactor errors for better UI rendering

--- controllers/users.go
+++ controllers/users.go
@@ -28,23 +28,7 @@ func NewUsers(us models.UserService) *Users {
 //
 // GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
-	type Alert struct {
-		Level   string
-		Message string
-	}
-	type Data struct {
-		Alert *Alert
-		Yield interface{}
-	}
-	alert := Alert{
-		Level:   "success",
-		Message: "Successfully rendered a dynamic alert!",
-	}
-	data := Data{
-		Alert: &alert,
-		Yield: "this can be any data b/c its type is interface",
-	}
-	if err := u.NewView.Render(w, data); err != nil {
+	if err := u.NewView.Render(w, nil); err != nil {
 		panic(err)
 	}
 }
@@ -53,9 +37,12 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 //
 // POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	var vd views.Data
 	var form SignupForm
 	if err := parseForm(r, &form); err != nil {
-		panic(err)
+		vd.SetAlert(err)
+		u.NewView.Render(w, vd)
+		return
 	}
 	// fmt.Fprintln(w, "Name is", form.Name)
 	// fmt.Fprintln(w, "Email is", form.Email)
@@ -67,15 +54,15 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 		Password: form.Password,
 	}
 	if err := u.us.Create(&user); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		vd.SetAlert(err)
+		u.NewView.Render(w, vd)
 		return
 	}
 	// fmt.Fprintln(w, "User is", user)
 
 	err := u.signIn(w, &user)
 	if err != nil {
-		// Temporarily render the error message for debugging
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Redirect(w, r, "/login", http.StatusFound)
 		return
 	}
 	// Redirect to the cookie test page to test the cookie
@@ -98,26 +85,29 @@ type LoginForm struct {
 //
 // POST /login
 func (u *Users) Login(w http.ResponseWriter, r *http.Request) {
-	form := LoginForm{}
+	var vd views.Data
+	var form LoginForm
 	if err := parseForm(r, &form); err != nil {
-		panic(err)
+		vd.SetAlert(err)
+		u.LoginView.Render(w, vd)
+		return
 	}
 	user, err := u.us.Authenticate(form.Email, form.Password)
 	if err != nil {
 		switch err {
 		case models.ErrNotFound:
-			fmt.Fprintln(w, "Invalid email address.")
-		case models.ErrPasswordIncorrect:
-			fmt.Fprintln(w, "Invalid password provided.")
+			vd.AlertError("No user exists with that email address")
 		default:
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			vd.SetAlert(err)
 		}
+		u.LoginView.Render(w, vd)
 		return
 	}
 
 	err = u.signIn(w, user)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		vd.SetAlert(err)
+		u.LoginView.Render(w, vd)
 		return
 	}
 	http.Redirect(w, r, "/cookietest", http.StatusFound)
--- models/users.go
+++ models/users.go
@@ -18,52 +18,65 @@ import (
 
 const hmacSecretKey = "secret-hmac-key"
 
-var (
+const (
 	userPwPepper = "secret-random-string"
 
 	// ErrNotFound is returned when a resource cannot be found
 	// in the database.
-	ErrNotFound = errors.New("models: resource not found")
+	ErrNotFound modelError = "models: resource not found"
 
 	// ErrIDInvalid is returned when an invalid ID is provided
 	// to a method like Delete.
-	ErrIDInvalid = errors.New("models: ID provided was invalid")
+	ErrIDInvalid modelError = "models: ID provided was invalid"
 
 	// ErrPasswordIncorrect is returned when an incorrect
 	// password is used when authenticating a user.
-	ErrPasswordIncorrect = errors.New("models: incorrect password provided")
-
-	// ErrEmailRequired is returned when an email address is
-	// not provided when creating a user
-	ErrEmailRequired = errors.New("models: email address is required")
-
-	// ErrEmailInvalid is returned when an email address provided
-	// does not match any of our requirements
-	ErrEmailInvalid = errors.New("models: email address is not valid")
-
-	// ErrEmailTaken is returned when an update or create is attempted
-	// with an email address that is already in use.
-	ErrEmailTaken = errors.New("models: email address is already taken")
+	ErrPasswordIncorrect modelError = "models: incorrect password provided"
 
 	// ErrPasswordTooShort is returned when a user tries to set
 	// a password that is less than 8 characters long.
-	ErrPasswordTooShort = errors.New("models: password must " +
-		"be at least 8 characters long")
+	ErrPasswordTooShort modelError = "models: password must " +
+		"be at least 8 characters long"
 
-	// ErrPasswordRequired is returned when a create is attempted
+		// ErrPasswordRequired is returned when a create is attempted
 	// without a user password provided.
-	ErrPasswordRequired = errors.New("models: password is required")
+	ErrPasswordRequired modelError = "models: password is required"
+
+	// ErrEmailRequired is returned when an email address is
+	// not provided when creating a user
+	ErrEmailRequired modelError = "models: email address is required"
+
+	// ErrEmailInvalid is returned when an email address provided
+	// does not match any of our requirements
+	ErrEmailInvalid modelError = "models: email address is not valid"
+
+	// ErrEmailTaken is returned when an update or create is attempted
+	// with an email address that is already in use.
+	ErrEmailTaken modelError = "models: email address is already taken"
 
 	// ErrRememberRequired is returned when a create or update
 	// is attempted without a user remember token hash
-	ErrRememberRequired = errors.New("models: remember token " +
-		"is required")
+	ErrRememberRequired modelError = "models: remember token " +
+		"is required"
 
 	// ErrRememberTooShort is returned when a remember token is
 	// not at least 32 bytes
-	ErrRememberTooShort = errors.New("models: remember token " +
-		"must be at least 32 bytes")
+	ErrRememberTooShort modelError = "models: remember token " +
+		"must be at least 32 bytes"
 )
+
+type modelError string
+
+func (e modelError) Error() string {
+	return string(e)
+}
+
+func (e modelError) Public() string {
+	s := strings.Replace(string(e), "models: ", "", 1)
+	split := strings.Split(s, " ")
+	split[0] = strings.Title(split[0])
+	return strings.Join(split, " ")
+}
 
 // UserService is a set of methods used to manipulate and
 // work with the user model
