786ba868ffd76956400f897b38dc1e1272f03cf3
luxcgo <luxcgo@gmail.com>
2021-04-01 23:02:40 +0800

    models/galleries: init

---
+++ models/galleries.go
@@ -0,0 +1,26 @@
+package models
+
+import "gorm.io/gorm"
+
+type Gallery struct {
+	gorm.Model
+	UserID uint   `gorm:"not null;index"`
+	Title  string `gorm:"not null"`
+}
+
+type GalleryService interface {
+	GalleryDB
+}
+
+type GalleryDB interface {
+	Create(gallery *Gallery) error
+}
+
+type galleryGorm struct {
+	db *gorm.DB
+}
+
+func (gg *galleryGorm) Create(gallery *Gallery) error {
+	// TODO: Implement this later
+	return nil
+}
