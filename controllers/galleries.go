e7168640015539d1d3541477e89cce5ab4cdd1c7
luxcgo <luxcgo@gmail.com>
2021-04-04 19:24:04 +0800

    controllers/galleries: add edit method

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -17,6 +17,7 @@ const (
 type Galleries struct {
 	New      *views.View
 	ShowView *views.View
+	EditView *views.View
 	gs       models.GalleryService
 	r        *mux.Router
 }
@@ -29,6 +30,7 @@ func NewGalleries(gs models.GalleryService, r *mux.Router) *Galleries {
 	return &Galleries{
 		New:      views.NewView("bootstrap", "galleries/new"),
 		ShowView: views.NewView("bootstrap", "galleries/show"),
+		EditView: views.NewView("bootstrap", "galleries/edit"),
 		gs:       gs,
 		r:        r,
 	}
@@ -66,12 +68,23 @@ func (g *Galleries) Create(w http.ResponseWriter, r *http.Request) {
 
 // GET /galleries/:id
 func (g *Galleries) Show(w http.ResponseWriter, r *http.Request) {
+	gallery, err := g.galleryByID(w, r)
+	if err != nil {
+		return
+	}
+	var vd views.Data
+	vd.Yield = gallery
+	g.ShowView.Render(w, vd)
+}
+
+func (g *Galleries) galleryByID(w http.ResponseWriter,
+	r *http.Request) (*models.Gallery, error) {
 	vars := mux.Vars(r)
 	idStr := vars["id"]
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
 		http.Error(w, "Invalid gallery ID", http.StatusNotFound)
-		return
+		return nil, err
 	}
 	gallery, err := g.gs.ByID(uint(id))
 	if err != nil {
@@ -79,12 +92,61 @@ func (g *Galleries) Show(w http.ResponseWriter, r *http.Request) {
 		case models.ErrNotFound:
 			http.Error(w, "Gallery not found", http.StatusNotFound)
 		default:
-			http.Error(w, "Whoops! Something went wrong.",
-				http.StatusInternalServerError)
+			http.Error(w, "Whoops! Something went wrong.", http.StatusInternalServerError)
 		}
+		return nil, err
+	}
+	return gallery, nil
+}
+
+// GET /galleries/:id/edit
+func (g *Galleries) Edit(w http.ResponseWriter, r *http.Request) {
+	gallery, err := g.galleryByID(w, r)
+	if err != nil {
+		return
+	}
+	// A user needs logged in to access this page, so we can
+	// assume that the RequireUser middleware has run and
+	// set the user for us in the request context.
+	user := context.User(r.Context())
+	if gallery.UserID != user.ID {
+		http.Error(w, "You do not have permission to edit "+
+			"this gallery", http.StatusForbidden)
 		return
 	}
 	var vd views.Data
 	vd.Yield = gallery
-	g.ShowView.Render(w, vd)
+	g.EditView.Render(w, vd)
+}
+
+func (g *Galleries) Update(w http.ResponseWriter, r *http.Request) {
+	gallery, err := g.galleryByID(w, r)
+	if err != nil {
+		return
+	}
+	user := context.User(r.Context())
+	if gallery.UserID != user.ID {
+		http.Error(w, "Gallery not found", http.StatusNotFound)
+		return
+	}
+
+	var vd views.Data
+	vd.Yield = gallery
+	var form GalleryForm
+	if err := parseForm(r, &form); err != nil {
+		// If there is an error we are going to render the
+		// EditView again with an alert message.
+		vd.SetAlert(err)
+		g.EditView.Render(w, vd)
+		return
+	}
+	gallery.Title = form.Title
+	// TODO: Persist this gallery change in the DB after
+	// we add an Update method to our GalleryService in the
+	// models package.
+	vd.Alert = &views.Alert{
+		Level:   views.AlertLvlSuccess,
+		Message: "Gallery updated successfully!",
+	}
+	g.EditView.Render(w, vd)
 }
